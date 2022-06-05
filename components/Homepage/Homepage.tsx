4a4568551fdf3e3adbf8d4d4ca1bc84b90372e85
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-06-06 00:17:16 +0545

    Fix bug in Navbar animation

--- components/Homepage/Homepage.tsx
+++ components/Homepage/Homepage.tsx
@@ -1,5 +1,6 @@
 import Layout from "components/Layout/Layout";
 import Head from "next/head";
+import Image from "next/image";
 import React from "react";
 import styles from "./Homepage.module.scss";
 
@@ -11,7 +12,28 @@ const Homepage = (props: Props) => {
       <Head>
         <title>Keshav Kishor Ram | Portfolio</title>
       </Head>
-      <div className={styles.homepage}>Homepage</div>
+      <div className={styles.homepage}>
+        <div className={styles.left}>
+          <h1>
+            Hello, I am <br />
+            <span className={`${styles.mark} ${styles.big}`}>
+              {" "}
+              Keshav Kishor Ram.
+            </span>{" "}
+            <br />I am a{" "}
+            <span className={styles.underline}>React / Next JS</span>
+            developer.
+          </h1>
+          <p>
+            I can build high quality and performant website for your business
+            using React / Next JS.
+          </p>
+          <button className={styles.hireMeBtn}>Hire me</button>
+        </div>
+        <div className={styles.right}>
+          <Image src={"/i1.png"} height={600} width={290} alt="illustration" />
+        </div>
+      </div>
     </Layout>
   );
 };
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,63 +1,57 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Navbar.module.scss";
 import { Icon } from "@iconify/react";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
-  const [isMounted, setMounted] = useState(false);
-  const navRef = useRef(isMounted ? document?.createElement("nav") : null);
   const [isCollapsed, setCollapsed] = useState(false);
 
+  const handleNavbarCollapse = () => {
+    if (window.scrollY <= 100) {
+      setCollapsed(false);
+      return;
+    }
+    setCollapsed(true);
+  };
   useEffect(() => {
-    setMounted(true);
-
-    document.addEventListener("scroll", (e) => {
-      if (window.scrollY <= 100) {
-        setCollapsed(false);
-        // console.log("less than 100");
-        // navRef.current?.classList.remove(styles.collapsed);
-        // navRef.current?.style.removeProperty("animation");
-        return;
-      }
-
-      setCollapsed(true);
-      // navRef.current?.classList.add(styles.collapsed);
-    });
+    handleNavbarCollapse();
+    document.addEventListener("scroll", handleNavbarCollapse);
 
     return () => {
-      setMounted(false);
+      document.removeEventListener("scroll", handleNavbarCollapse);
     };
   }, []);
 
   return (
-    <nav
-      className={`${styles.navbar} ${isCollapsed && styles.collapsed}`}
-      ref={navRef}
-    >
-      <div className={styles.navbarLogo}>
-        <div className={styles.first}></div>
-        <div className={styles.second}></div>
-      </div>
+    <>
+      {isCollapsed && (
+        <div className={styles.collapsedNav}>
+          <Icon icon="ci:menu-alt-02" className={styles.menuIcon} />
+          <ul className={styles.collapsedNavList}>
+            <li className={styles.collapsedNavItem}>Home</li>
+            <li className={styles.collapsedNavItem}>Skills</li>
+            <li className={styles.collapsedNavItem}>Projects</li>
+            <li className={styles.collapsedNavItem}>About</li>
+            <li className={styles.collapsedNavItem}>Contact</li>
+          </ul>
+        </div>
+      )}
+      <div className={styles.nav1}>
+        <div className={styles.navbarLogo}>
+          <div className={styles.first}></div>
+          <div className={styles.second}></div>
+        </div>
 
-      <ul className={styles.navbarNav}>
-        <li className={styles.navItem}>Home</li>
-        <li className={styles.navItem}>Skills</li>
-        <li className={styles.navItem}>Projects</li>
-        <li className={styles.navItem}>About</li>
-        <li className={styles.navItem}>Contact</li>
-      </ul>
-      <div className={styles.collapsedNav}>
-        <Icon icon="ci:menu-alt-02" className={styles.menuIcon} />
-        <ul className={styles.collapsedNavList}>
-          <li className={styles.collapsedNavItem}>Home</li>
-          <li className={styles.collapsedNavItem}>Skills</li>
-          <li className={styles.collapsedNavItem}>Projects</li>
-          <li className={styles.collapsedNavItem}>About</li>
-          <li className={styles.collapsedNavItem}>Contact</li>
+        <ul className={styles.navbarNav}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem}>Skills</li>
+          <li className={styles.navItem}>Projects</li>
+          <li className={styles.navItem}>About</li>
+          <li className={styles.navItem}>Contact</li>
         </ul>
       </div>
-    </nav>
+    </>
   );
 };
 
